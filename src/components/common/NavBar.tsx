08529a7ca64348ebe4850423e07a8658bd87fa46
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-06-12 23:11:53 -0400

    fix: nav bar refactoring

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,78 +1,60 @@
 import * as React from 'react';
-import { StatefulTabs, Tab } from 'baseui/tabs';
-import { Block } from 'baseui/block';
+import { NavLink } from 'react-router-dom';
 import ChevronRightIcon from 'baseui/icon/chevron-right';
 
 import homeIcon from '../../images/home.png';
-
-const tabOutlineColor = { Tab: { style: { outlineColor: '#ffffff00' } } };
 
 interface Props {
 	history: { push: (string) => void; location: { pathname: string } };
 }
 
 class NavBar extends React.Component<Props> {
-	renderAdminNavTab = () => (
-		<Block style={{ display: 'flex' }}>
-			<span>System Admin</span>
-			<Block style={{ paddingTop: '10px' }}>
-				<ChevronRightIcon size={35} />
-			</Block>
-		</Block>
+	renderDashboardNav = () => (
+		<div role="tablist">
+			<NavLink activeClassName="selected" role="tab" to="/dashboard/accounts">
+				Accounts
+			</NavLink>
+			<NavLink activeClassName="selected" role="tab" to="/dashboard/agents">
+				Agents
+			</NavLink>
+			<NavLink activeClassName="selected" role="tab" to="/dashboard/sysadmins">
+				System Admins
+			</NavLink>
+		</div>
 	);
 
-	renderDashboardNav = push => (
-		<StatefulTabs initialState={{ activeKey: '0' }}>
-			<Tab
-				overrides={tabOutlineColor}
-				title="Accounts"
-				onClick={() => push('/dashboard/accounts')}
-			/>
-			<Tab
-				overrides={tabOutlineColor}
-				title="Agents"
-				onClick={() => push('/dashboard/agents')}
-			/>
-			<Tab
-				overrides={tabOutlineColor}
-				title="System Admins"
-				onClick={() => push('/dashboard/sysadmins')}
-			/>
-		</StatefulTabs>
-	);
-
-	renderAdminNavBar = push => (
-		<StatefulTabs initialState={{ activeKey: '2' }}>
-			<Tab
-				overrides={tabOutlineColor}
-				title={<img className="nav-icon" src={homeIcon} />}
-				onClick={() => push(`/dashboard/accounts`)}
-			/>
-			<Tab
-				disabled={true}
-				overrides={{
-					Tab: {
-						style: { cursor: 'context-menu', outlineColor: '#ffffff00' }
-					}
-				}}
-				title={this.renderAdminNavTab()}
-			/>
-			<Tab
-				overrides={tabOutlineColor}
-				title="Profile Overview"
-				onClick={() => push(`/dashboard/adminid/info`)}
-			/>
-			<Tab
-				overrides={tabOutlineColor}
-				title="Activity"
-				onClick={() => push(`/dashboard/adminid/activity`)}
-			/>
-		</StatefulTabs>
+	renderAdminNavBar = () => (
+		<div role="tablist">
+			<NavLink activeClassName="selected" role="tab" to="/dashboard/accounts">
+				<img className="nav-icon-home" src={homeIcon} />
+			</NavLink>
+			<NavLink role="tab" to="#">
+				<div className="nav-title">
+					<span>System Admin</span>
+					<div className="nav-icon">
+						<ChevronRightIcon size={35} />
+					</div>
+				</div>
+			</NavLink>
+			<NavLink
+				activeClassName="selected"
+				role="tab"
+				to="/dashboard/adminid/info"
+			>
+				Profile Overview
+			</NavLink>
+			<NavLink
+				activeClassName="selected"
+				role="tab"
+				to="/dashboard/adminid/activity"
+			>
+				Activity
+			</NavLink>
+		</div>
 	);
 
 	render() {
 		const {
-			history: { push },
 			history: {
 				location: { pathname }
 			}
@@ -83,8 +65,8 @@ class NavBar extends React.Component<Props> {
 					{pathname === '/dashboard/accounts' ||
 					pathname === '/dashboard/agents' ||
 					pathname === '/dashboard/sysadmins'
-						? this.renderDashboardNav(push)
-						: this.renderAdminNavBar(push)}
+						? this.renderDashboardNav()
+						: this.renderAdminNavBar()}
 				</div>
 			</div>
 		);
